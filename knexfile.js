f4f6d42cd65ee8fcc2d0f5251519694e3843baf0
Qerkules <mahammad.sarkarli@gmail.com>
2025-07-07 12:39:05 +0400

    Update db

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,26 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    client: 'mssql',
+    client: "mssql",
     connection: {
-      host: '31.186.13.154',
+      host: "31.186.13.154",
       port: 52332,
-      user: 'manager',
-      password: 'FidTbp5Rmv3sh8',
-      database: 'admin_ibsa',
+      user: "manager",
+      password: "FidTbp5Rmv3sh8",
+      database: "admin_ibsa",
       options: {
         trustServerCertificate: true,
         encrypt: true,
       },
     },
+    pool: {
+      min: 1,
+      max: 10,
+      idleTimeoutMillis: 1001 * 60, // Bağlantılar boşta kalsa bile kapanmasın
+    },
     migrations: {
-      directory: './migrations',
+      directory: "./migrations",
     },
   },
 };
